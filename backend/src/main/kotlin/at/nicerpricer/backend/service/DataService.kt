8ab3ab509de700d6f626ec9492893a912334ee08
abl <ableimat@gmx.at>
2023-07-29 21:30:22 +0200

    apache lucene search

--- backend/src/main/kotlin/at/nicerpricer/backend/service/DataService.kt
+++ backend/src/main/kotlin/at/nicerpricer/backend/service/DataService.kt
@@ -7,9 +7,22 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.de.GermanAnalyzer
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.TextField
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.index.Term
+import org.apache.lucene.search.*
+import org.apache.lucene.store.ByteBuffersDirectory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.File
+import java.io.StringReader
 
 val JACKSON_MAPPER: ObjectMapper = ObjectMapper()
     .registerModule(KotlinModule.Builder().build())
@@ -19,19 +32,64 @@ val JACKSON_MAPPER: ObjectMapper = ObjectMapper()
 class DataService(
     @Value("\${nicerpricer.file.path}") filePath: String,
 ) {
-    private val data: List<Data> = loadData(filePath)
+
+    private val data: List<Data>
+    private val indexSearcher: IndexSearcher
+    private val analyzer: Analyzer
+
+    init {
+        data = loadData(filePath)
+
+        val directory = ByteBuffersDirectory()
+        analyzer = GermanAnalyzer()
+        IndexWriter(directory, IndexWriterConfig(analyzer)).use {
+            indexData(it, data)
+        }
+
+        val directoryReader = DirectoryReader.open(directory)
+        indexSearcher = IndexSearcher(directoryReader)
+    }
 
     fun first(): Data {
         return data.first()
     }
 
-    fun query(nameQuery: String): List<String> {
-        return data.asSequence()
-            .mapNotNull { it.name }
-            .filter { it.contains(nameQuery, true) }
-            .distinctBy { it.lowercase() }
+    fun query(names: String): List<String> {
+        val query = buildQuery(names)
+        val result = indexSearcher.search(query, 40)
+        val storedFields = indexSearcher.storedFields()
+        return result
+            .scoreDocs
+            .sortedBy { it.score }
+            .reversed()
+            .map { storedFields.document(it.doc).getField("name").stringValue() }
             .take(10)
-            .toList()
+    }
+
+    private fun buildQuery(names: String): Query {
+        val builder = BooleanQuery.Builder()
+        analyzer.tokenStream("name", StringReader(names))
+            .use { tokenStream ->
+                val offsetAttribute = tokenStream.addAttribute(OffsetAttribute::class.java)
+                tokenStream.reset()
+
+                while (tokenStream.incrementToken()) {
+                    val token = names.substring(
+                        offsetAttribute.startOffset(),
+                        offsetAttribute.endOffset()
+                    )
+                    builder.add(
+                        BooleanClause(
+                            WildcardQuery(
+                                Term("name", "$token*")
+                            ),
+                            BooleanClause.Occur.MUST
+                        )
+                    )
+                }
+            }
+
+        return builder.build()
     }
 
     fun shop(groceryList: GroceryList): ShoppingTrip {
@@ -42,5 +100,17 @@ class DataService(
         return JACKSON_MAPPER.readValue(
             File(filePath),
             object : TypeReference<List<Data>>() {})
+    }
+
+    private fun indexData(indexWriter: IndexWriter, data: List<Data>) {
+        val distinctNames = data
+            .map { it.name }
+            .distinct()
+        
+        for (name in distinctNames) {
+            val doc = Document()
+            doc.add(TextField("name", name, Field.Store.YES))
+            indexWriter.addDocument(doc)
+        }
     }
 }
