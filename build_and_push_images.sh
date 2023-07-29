fb5fb088d09c4097629ca4ed9ea5c0e610f6a12e
abl <ableimat@gmx.at>
2023-07-29 20:13:46 +0200

    deploy frontend

--- build_and_push_images.sh
+++ build_and_push_images.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+set -e
 
 #build and push backend
 cd backend
 ./gradlew bootJar
 docker build -t registry.slothyx.com/nicerpricer-backend .
 docker push registry.slothyx.com/nicerpricer-backend
+cd ..
+
+#build and push frontend
+cd frontend
+docker build -t registry.slothyx.com/nicerpricer-frontend .
+docker push registry.slothyx.com/nicerpricer-frontend
 cd ..
